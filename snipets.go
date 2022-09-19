a3769bf15cc2bc702d3a2dde42094514ae8ddd16
Eduardo Gordillo <94454054+fluty84@users.noreply.github.com>
2022-09-19 16:39:55 +0200

    Update snipets.go

--- snipets.go
+++ snipets.go
@@ -116,3 +116,34 @@ func main() {
 	pic.Show(Pic)
 
 }
+
+Exercise: Maps
+Implement WordCount. It should return a map of the counts of each “word” in the string s. The wc.Test function runs a test suite against the provided function and prints success or failure.
+
+You might find strings.Fields helpful.
+
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	//"fmt"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	
+	sliced := strings.Fields(s)
+	
+	countMap := make(map[string]int)
+	
+	for _, value := range sliced {
+	
+		countMap[value] += 1
+	}
+	
+	return countMap
+}
+
+func main() {
+	wc.Test(WordCount)
+}
