21158645d4ccdf33a90751f05aee28dd552d190f
Eduardo Gordillo <94454054+fluty84@users.noreply.github.com>
2022-09-19 13:37:37 +0200

    adding Tour of Go second exercise

--- snipets.go
+++ snipets.go
@@ -78,3 +78,41 @@ func SquareSum(numbers []int) int {
   }
   return sum
 }
+
+Exercise: Slices
+Implement Pic. It should return a slice of length dy, each element of which is a slice of dx 8-bit unsigned integers. When you run the program, it will display your picture, interpreting the integers as grayscale (well, bluescale) values.
+
+The choice of image is up to you. Interesting functions include (x+y)/2, x*y, and x^y.
+
+(You need to use a loop to allocate each []uint8 inside the [][]uint8.)
+
+(Use uint8(intValue) to convert between types.)
+
+package main
+
+import (
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+
+	picture := make([][]uint8, dy)
+
+	for y, _ := range picture {
+
+		data := make([]uint8, dx)
+
+		for x, _ := range data {
+			data[x] = uint8(x ^ y)
+		}
+		
+		picture[y] = data
+	}
+
+	return picture
+}
+
+func main() {
+	pic.Show(Pic)
+
+}
