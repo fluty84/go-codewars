1ac0dd66fb0ed7f290c4ed62b042f7251f71c4a2
Eduardo Gordillo <94454054+fluty84@users.noreply.github.com>
2022-09-20 16:46:49 +0200

    Update snipets.go

--- snipets.go
+++ snipets.go
@@ -188,4 +188,33 @@ func main() {
 	}
 }
 
+Exercise: Errors
+Copy your Sqrt function from the earlier exercise and modify it to return an error value.
+
+Sqrt should return a non-nil error value when given a negative number, as it doesn't support complex numbers.
+
+package main
+
+import (
+	"fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string{
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+	}
+
+func Sqrt(x float64) (float64, error) {
+	
+	if x < 0 {
+	return 0, ErrNegativeSqrt(x)
+	}
+	z := 1.0
+	
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z, nil
+}
 
