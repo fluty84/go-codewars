ed4148182982533a1fbf931ced21d0ed48e92f5d
Eduardo Gordillo <94454054+fluty84@users.noreply.github.com>
2022-09-21 11:46:31 +0200

    exercise rot13reader

--- snipets.go
+++ snipets.go
@@ -239,3 +239,56 @@ func main() {
 	reader.Validate(MyReader{})
 }
 
+Exercise: rot13Reader
+A common pattern is an io.Reader that wraps another io.Reader, modifying the stream in some way.
+
+For example, the gzip.NewReader function takes an io.Reader (a stream of compressed data) and returns a *gzip.Reader that also implements io.Reader (a stream of the decompressed data).
+
+Implement a rot13Reader that implements io.Reader and reads from an io.Reader, modifying the stream by applying the rot13 substitution cipher to all alphabetical characters.
+
+The rot13Reader type is provided for you. Make it an io.Reader by implementing its Read method.
+
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (payload *rot13Reader) Read(b []byte) (n int, err error) {
+	
+	n, err = payload.r.Read(b)
+	
+	for i := range b {
+	
+		if b[i] >= 'a' && b[i] <= 'z' {
+        // Rotate lowercase letters 13 places.
+        	if b[i] >= 'm' {
+            b[i] = b[i] - 13
+        	} else {
+            b[i] = b[i] + 13
+        	}
+    	}else if b[i] >= 'A' && b[i] <= 'Z'{ 
+			// Rotate upercase letters 13 places.
+        	if b[i] >= 'M' {
+            b[i] = b[i] - 13
+        	} else {
+            b[i] = b[i] + 13
+        	}
+		}
+	
+	}
+	return 
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
+
