febf83aa0020511470f634709f50a4b4fe39f79a
Eduardo Gordillo <94454054+fluty84@users.noreply.github.com>
2022-09-26 13:06:35 +0200

    adding exercise Interest is interesting
     from Go on Exercism

--- snipets.go
+++ snipets.go
@@ -421,4 +421,93 @@ func main() {
 
 }
 
+/* Instructions
+In this exercise you'll be working with savings accounts. Each year, the balance of your savings account is updated based on its interest rate. The interest rate your bank gives you depends on the amount of money in your account (its balance):
+
+3.213% for a balance less than 0 dollars (balance gets more negative).
+0.5% for a balance greater than or equal to 0 dollars, and less than 1000 dollars.
+1.621% for a balance greater than or equal to 1000 dollars, and less than 5000 dollars.
+2.475% for a balance greater than or equal to 5000 dollars.
+You have four tasks, each of which will deal your balance and its interest rate. */
+
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+    switch {
+        case balance < 0:
+    	return 3.213
+        case balance < 1000:
+    	return 0.5
+        case balance < 5000:
+    	return 1.621
+        default:
+    	return 2.475
+    }
+	panic("Please implement the InterestRate function")
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+    switch {
+        case balance < 0:
+    	return balance * 0.03213
+        case balance < 1000:
+    	return balance * 0.005
+        case balance < 5000:
+    	return balance * 0.01621
+        default:
+    	return balance * 0.02475
+    }
+	panic("Please implement the Interest function")
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+    switch {
+        case balance < 0:
+    	return balance * 0.03213 + balance
+        case balance < 1000:
+    	return balance * 0.005 + balance
+        case balance < 5000:
+    	return balance * 0.01621 + balance
+        default:
+    	return balance * 0.02475 + balance
+    }
+	panic("Please implement the AnnualBalanceUpdate function")
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+    
+    totalYears := 0 
+
+    var AnnualBalanceUpdate func(balance float64, targetBalance float64) int 
+    
+    AnnualBalanceUpdate = func(balance float64, targetBalance float64) int {
+    
+    if balance > targetBalance {
+        return totalYears
+    }
+    
+	totalYears++
+    switch {
+        case balance < 0:
+    	balance = balance * 0.03213 + balance
+        case balance < 1000:
+    	balance = balance * 0.005 + balance
+        case balance < 5000:
+    	balance = balance * 0.01621 + balance
+        default:
+    	balance = balance * 0.02475 + balance
+    }
+    return AnnualBalanceUpdate(balance, targetBalance)
+    }
+AnnualBalanceUpdate(balance, targetBalance)
+    
+return totalYears
+    
+	panic("Please implement the YearsBeforeDesiredBalance function")
+}
+
 
